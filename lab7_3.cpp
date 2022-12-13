a718f5a53caee176f6d7bcb5d323dd60e4868032
Kittiwin Phannachet <kittiwin_phannachet@cmu.ac.th>
2022-12-13 17:10:43 +0700

    Lab7_3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,13 +3,37 @@
 using namespace std;
 
 
+
+int adiff(int x, int y){
+
+  int ans;
+  
+  if(x < 0){
+    x = x + 360;
+  }  
+  if(y < 0){
+    y = y + 360;
+  }
+  x = x%360;
+  y = y%360;
+  ans = x - y;
+  if(ans < 0){
+    ans *= -1;
+  }
+  if(ans > 180){
+    ans = 360 - ans;
+  }
+  
+  return ans ;
+}
+
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244) << endl;
 }
